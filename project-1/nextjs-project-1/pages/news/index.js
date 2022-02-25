2419759ef6fb2590e17924de1906c4e564f971fc
James McCarron <mccarron.j@gmail.com>
2022-02-25 14:06:10 +1100

    Linking between pages example in index.js

--- project-1/nextjs-project-1/pages/news/index.js
+++ project-1/nextjs-project-1/pages/news/index.js
@@ -1,9 +1,21 @@
 // our-domain.com/news
-
-import React from "react";
+import Link from "next/link";
+import { Fragment } from "react";
 
 const NewsPage = () => {
-  return <h1> The News Page</h1>;
+  return (
+    <Fragment>
+      <h1> The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">
+            NextJS is a great framework!!
+          </Link>
+        </li>
+        <li>NextJS is the best!</li>
+      </ul>
+    </Fragment>
+  );
 };
 
 export default NewsPage;
