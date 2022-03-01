ec47e0646a14efda6c207f5151a112c28c31a47d
James McCarron <mccarron.j@gmail.com>
2022-03-01 16:10:56 +1100

    Used getStaticProps & getStaticPaths in [meetupId]

--- project-2/meetup-app/pages/[meetupId]/index.js
+++ project-2/meetup-app/pages/[meetupId]/index.js
@@ -13,4 +13,43 @@ const MeetupDetails = () => {
   );
 };
 
+export const getStaticPaths = async () => {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+};
+
+export const getStaticProps = async (context) => {
+  // fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Sydney_Opera_House%2C_vivid_Sydey.JPG/2880px-Sydney_Opera_House%2C_vivid_Sydey.JPG",
+        id: meetupId,
+        title: "First Meetup",
+        address: "Some Street 7, Some City",
+        description: "This is a first meetup",
+      },
+    },
+  };
+};
+
 export default MeetupDetails;
