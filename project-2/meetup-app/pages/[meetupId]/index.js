2d9fb24794e448e4a4144a12258295ecd3ef1a5f
James McCarron <mccarron.j@gmail.com>
2022-03-01 12:17:03 +1100

    Added custom meetup detail component

---
+++ project-2/meetup-app/pages/[meetupId]/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Sydney_Opera_House%2C_vivid_Sydey.JPG/2880px-Sydney_Opera_House%2C_vivid_Sydey.JPG"
+      title="First Meetup"
+      address="Some Street 7, Some City"
+      description="This is a first meetup"
+    />
+  );
+};
+
+export default MeetupDetails;
