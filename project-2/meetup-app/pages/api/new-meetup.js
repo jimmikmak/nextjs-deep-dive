e6ab48928da0b4ca568fd201d6095cc207a28889
James McCarron <mccarron.j@gmail.com>
2022-03-01 17:24:37 +1100

    Added new api folder w/ handler function for requests

---
+++ project-2/meetup-app/pages/api/new-meetup.js
@@ -0,0 +1,12 @@
+// /api/new-meet-up
+// POST /api/new-meetup
+
+const handler = (req, res) => {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const { title, image, address, description } = data;
+  }
+};
+
+export default handler;
