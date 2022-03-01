520994491535a60ba54c0e29f471c3339b03adf4
James McCarron <mccarron.j@gmail.com>
2022-03-01 15:35:01 +1100

    getServerSideProps example in index.js

--- project-2/meetup-app/pages/index.js
+++ project-2/meetup-app/pages/index.js
@@ -8,7 +8,7 @@ const DUMMY_MEETUPS = [
     title: "A First Meetup",
     image:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Sydney_Opera_House%2C_vivid_Sydey.JPG/2880px-Sydney_Opera_House%2C_vivid_Sydey.JPG",
-    address: "An address 5, 12345 A City",
+    address: "Some Address 5, 12345 A City",
     description: "This is a first meet-up!",
   },
   {
@@ -16,7 +16,7 @@ const DUMMY_MEETUPS = [
     title: "A Second Meetup",
     image:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Sydney_%28AU%29%2C_Harbour_Bridge_--_2019_--_2179.jpg/2880px-Sydney_%28AU%29%2C_Harbour_Bridge_--_2019_--_2179.jpg",
-    address: "An address 5, 12345 A City",
+    address: "Some Address 7, 56789 A City",
     description: "This is a second meet-up!",
   },
 ];
@@ -25,12 +25,24 @@ const HomePage = (props) => {
   return <MeetupList meetups={props.meetups} />;
 };
 
+// export const getServerSideProps = async (context) => {
+//   const req = context.request;
+//   const res = context.response;
+
+//   // fetch data from an API
+
+//   return {
+//     props: { DUMMY_MEETUPS },
+//   };
+// };
+
 export const getStaticProps = async () => {
   // fetch data from an API
   return {
     props: {
       meetups: DUMMY_MEETUPS,
     },
+    revalidate: 10,
   };
 };
 
